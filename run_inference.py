bfe466854a5da6fe1a2fca10186f42576233c723
Artem <atuysuzov@gmail.com>
2019-10-24 17:08:16 +0300

    running inference

---
+++ run_inference.py
@@ -0,0 +1,61 @@
+import pickle
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models import SimpleLSTM
+from dataloader import Vocab, tokenize
+
+
+def make_predictions(
+    model: nn.Module,
+    vocab: Vocab,
+    text: str,
+    max_len: int = 300,
+    device: str = "cpu",
+):
+
+    tokens = tokenize(text, remove_punct=False, mode="simple")
+    tokens_idx = [vocab[token] for token in tokens]
+    X = torch.LongTensor(tokens_idx[:max_len])
+    X_len = torch.ShortTensor([X.shape[0]])
+    X = F.pad(X, (0, max_len - X.shape[0])).to(device)
+    X = X.unsqueeze(0)
+
+    predictions = model(X, X_len)
+    return predictions
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model_dictionary = torch.load("outputs/models/simple_lstm_16.pth", map_location=device)
+
+model = SimpleLSTM(**model_dictionary["model_args"])
+
+model.load_state_dict(model_dictionary["model_weights"])
+model.to(device)
+model.eval()
+
+with open("outputs/vocab/overviews_vocab.pcl", "rb") as f:
+    overviews_vocab = pickle.load(f)
+with open("outputs/vocab/genres_vocab.pcl", "rb") as f:
+    genres_vocab = pickle.load(f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--text", default="This is some funny story")
+    parser.add_argument("--threshold", type=float, default=0.5)
+
+    args = parser.parse_args()
+    text = args.text
+    threshold = args.threshold
+
+    predictions = make_predictions(model, vocab=overviews_vocab, text=text)
+    genre_indices = (torch.sigmoid(predictions.squeeze()) > threshold).nonzero().squeeze().tolist()
+
+    predicted_genres = [genres_vocab[ix] for ix in genre_indices]
+    print(text)
+    print(f'Predicted: {", ".join(predicted_genres)}')
